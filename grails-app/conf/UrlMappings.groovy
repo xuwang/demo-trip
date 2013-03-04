88296a0736412d9ebc8aa45c22d2c6db692d7ab5
Xu Wang <xuwnag@gmail.com>
2013-03-04 15:36:46 -0800

    add url mapping for airport/json and airport/geocode

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,24 @@
 class UrlMappings {
 
 	static mappings = {
+		
+		"/airport/json?/$iata?"(controller: 'airport', action:'getJson'){
+			constraints {
+				// apply constraints here
+			}
+		}
+		
+		"/airport/geocode?/$iata?"(controller: 'airport', action:'geocode'){
+			constraints {
+				// apply constraints here
+			}
+		}
+		"/$controller/$action?/$id?"{
+			constraints {
+				// apply constraints here
+			}
+		}
+		
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
